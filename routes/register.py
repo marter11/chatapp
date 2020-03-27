7db8a2e42308c130a915748a9880424a1b092154
leventecsoba <csobal26@gmail.com>
2020-03-27 17:18:57 +0100

    User registration basics added

--- routes/register.py
+++ routes/register.py
@@ -1,5 +1,17 @@
 from . import app, render_template
+from models.user_model import UserModel, UserModelHandler
+from flask import request
 
-@app.route("/register")
+@app.route("/register", methods=['POST'])
 def register_view():
     return render_template("/authentication/register.html")
+
+def UserReg():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        UserModelHandler(username, email, password)
+        UserModelHandler.set()
+        UserModelHandler.save()
