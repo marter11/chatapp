657368bdce5b97875254523059f12d7477c3367c
marter11 <unknown@mail.com>
2020-04-05 12:45:22 +0300

    Bugfix and extra features

--- static/room_related/room_menu_additional.js
+++ static/room_related/room_menu_additional.js
@@ -1,15 +1,3 @@
-// function InitalizeRoomOptions(e) {
-//   let html_element =
-//   `<span class="options-popup">
-//     <span>Change</span>
-//     <span>Delete</span>
-//   </span>`;
-//
-//   if(e.target.tagName == "I") {
-//     e.target.innerHTML = html_element;
-//   }
-// }
-
 function ToggleRoomOptions(e) {
   e.target.classList.toggle("appear");
 }
